6d714d94593a89d5b270aa670a3dbd2ed0d8a3a6
evanroane <evan.roane@gmail.com>
2014-12-05 15:13:32 -0600

    working basic address book using angular

--- js/main.js
+++ js/main.js
@@ -2,9 +2,35 @@
   'use strict';
 
   angular.module("ab", [])
-    .controller('abController', function(){
-      var x = this
-    });
-    
+    .controller('AbController', function(){
+      var vm = this;
+      vm.persons = [
+      {
+        firstName: 'Evan',
+        lastName: 'Roane',
+        address: '192.168.1.1',
+        email: 'evan.roane@gmail.com',
+        github: 'github.com/evanroane',
+        twitter: '@roaneZone'
+      }
+    ];
 
+    vm.addNewPerson = function(){
+      vm.persons.push(vm.newPerson);
+      vm.newPerson = _newPerson();
+    };
+
+    vm.removePerson = function(person){
+      var index = vm.persons.indexOf(person);
+      vm.persons.splice(index,1);
+    };
+
+    vm.newPerson = _newPerson();
+
+    function _newPerson(){
+      return {
+        twitter: '@'
+      }
+    }
+  });
 }());
